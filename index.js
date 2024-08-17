ff218c1a316e99a21fdde37d76532d05e3b1d4f1
unknown <rasalila@list.ru>
2024-08-17 09:59:50 +0300

    deleted not used imgs

---
+++ index.js
@@ -0,0 +1,11 @@
+alert("Я учу JavaScript!");
+
+let image = document.getElementById("myImage");
+console.log(image1);
+
+function changeImgSrc() {
+  image.src = "./assets/img/image2.jpg";
+}
+function changeImgSrc2() {
+  image.src = "./assets/img/image1.jpg";
+}
